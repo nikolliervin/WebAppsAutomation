927a040332e3ab55ccbc4499889506183ed98a21
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-08 00:55:45 +0200

    TruthOrDare Feature - Launching the app - Launch the app method

--- WebAppsAutomation/StepDefinitions/TruthOrDareGameSteps.cs
+++ WebAppsAutomation/StepDefinitions/TruthOrDareGameSteps.cs
@@ -7,11 +7,19 @@ namespace WebAppsAutomation.StepDefinitions
 {
     public class TruthOrDareGameSteps
     {
+        private readonly TruthOrDareGame truthOrDare;
+        public TruthOrDareGameSteps(TruthOrDareGame _truthOrDare)
+        {
+            truthOrDare = _truthOrDare; 
+        }
+
         [Given(@"I have launched the TruthOrDare game")]
         public void GivenIHaveLaunchedTheTruthOrDareGame()
         {
-            throw new PendingStepException();
+            truthOrDare.IHaveLaunchedTheApp();
         }
+
+
 
     }
 }
--- WebAppsAutomation/Steps/TruthOrDareGame.cs
+++ WebAppsAutomation/Steps/TruthOrDareGame.cs
@@ -5,10 +5,23 @@ using System.Collections.Generic;
 using System.Text;
 using FluentAssertions;
 using WebAppsAutomation.Page_Elements;
+using WebAppsAutomation.PageElements;
 
 namespace WebAppsAutomation.Steps
 {
     public class TruthOrDareGame
     {
+        IWebDriver webDriver = new ChromeDriver();
+        private readonly TruthOrDareGameElements truthOrDareMap;
+        public TruthOrDareGame(TruthOrDareGameElements _truthOrDareMap)
+        {
+            truthOrDareMap = _truthOrDareMap;
+        }
+
+        public void IHaveLaunchedTheApp()
+        {
+            webDriver.Navigate().GoToUrl("https://boisterous-froyo-aacb2d.netlify.app/");
+        }
+
     }
 }
