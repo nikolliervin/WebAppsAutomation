fc9c0f4a1c805d2cfa7d03cb989fb8d640b5e4ec
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-08 00:39:55 +0200

    Adjusted libraries for TruthOrDareGame.cs

--- WebAppsAutomation/Steps/TruthOrDareGame.cs
+++ WebAppsAutomation/Steps/TruthOrDareGame.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentAssertions;
+using WebAppsAutomation.Page_Elements;
 
 namespace WebAppsAutomation.Steps
 {
-    internal class TruthOrDareGame
+    public class TruthOrDareGame
     {
     }
 }
