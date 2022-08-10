a8f0afa759cd235af07ad8e8a46bbfa5858d23b8
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-10 02:54:25 +0200

    Added Hooks file

---
+++ WebAppsAutomation/Hooks/Hooks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using WebAppsAutomation.Steps;
+using WebAppsAutomation.PageElements;
+using OpenQA.Selenium.Chrome;
+
+namespace WebAppsAutomation.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+
+    }
+}
