3f4c9885ffee4c4ab9198c158fd7ae478b745835
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-12 01:13:07 +0200

    Stopwatch Feature - Launching the stopwatch and checking components - Generic Xpath for all elements

--- WebAppsAutomation/PageElements/StopwatchElements.cs
+++ WebAppsAutomation/PageElements/StopwatchElements.cs
@@ -1,9 +1,10 @@
-﻿namespace WebAppsAutomation.Pages
+﻿using OpenQA.Selenium;
+
+namespace WebAppsAutomation.Pages
 {
     public partial class StopwatchElements
     {
-
-
+        public By Element(string option) => By.XPath($"//*[@id='{option}']");
 
 
     }
