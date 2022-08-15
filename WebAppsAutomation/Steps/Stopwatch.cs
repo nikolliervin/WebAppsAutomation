baa3030f24ccb6202772752f3a7b15b839b2a157
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-15 20:05:21 +0200

    WebDriver Refactoring for Stopwatch.cs

--- WebAppsAutomation/Steps/Stopwatch.cs
+++ WebAppsAutomation/Steps/Stopwatch.cs
@@ -1,14 +1,11 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using WebAppsAutomation.Pages;
 
 namespace WebAppsAutomation.Steps
 {
-    public class Stopwatch
+    public class Stopwatch : WebDriver.WebDriver
     {
-        IWebDriver webDriver = new ChromeDriver();
         private readonly StopwatchElements stopwatchElements;
         public Stopwatch(StopwatchElements _stopwatchElements)
         {
@@ -17,28 +14,28 @@ namespace WebAppsAutomation.Steps
 
         public void IHaveLaunchedTheStopwatch()
         {
-            webDriver.Navigate().GoToUrl("https://courageous-dusk-90082b.netlify.app/");
+            driver.Navigate().GoToUrl("https://courageous-dusk-90082b.netlify.app/");
         }
 
         public bool ThenTheComponentsAreShown(List<string> components)
         {
-            return webDriver.FindElement(stopwatchElements.Element(components[0].ToLower())).Displayed &&
-                   webDriver.FindElement(stopwatchElements.Element(components[1].ToLower())).Displayed &&
-                   webDriver.FindElement(stopwatchElements.Element(components[2].ToLower())).Displayed &&
-                   webDriver.FindElement(stopwatchElements.Element(components[3].ToLower())).Displayed;
+            return driver.FindElement(stopwatchElements.Element(components[0].ToLower())).Displayed &&
+                   driver.FindElement(stopwatchElements.Element(components[1].ToLower())).Displayed &&
+                   driver.FindElement(stopwatchElements.Element(components[2].ToLower())).Displayed &&
+                   driver.FindElement(stopwatchElements.Element(components[3].ToLower())).Displayed;
 
         }
 
         public void IClickTheStopwatchButton(string button)
         {
-            webDriver.FindElement(stopwatchElements.Element(button)).Click();
+            driver.FindElement(stopwatchElements.Element(button)).Click();
         }
 
         public bool ThenTheStopwatchStarts()
         {
             Thread.Sleep(2000);
-            webDriver.FindElement(stopwatchElements.Element("stop")).Click();
-            return webDriver.FindElement(stopwatchElements.Element("timer")).Text != "00:00:00";
+            driver.FindElement(stopwatchElements.Element("stop")).Click();
+            return driver.FindElement(stopwatchElements.Element("timer")).Text != "00:00:00";
         }
 
         public void ThenIWaitSeconds(int seconds)
@@ -48,7 +45,7 @@ namespace WebAppsAutomation.Steps
 
         public bool ThenTheStopwatchDisplays(string field, string value)
         {
-            return webDriver.FindElement(stopwatchElements.Element(field)).Text == value;
+            return driver.FindElement(stopwatchElements.Element(field)).Text == value;
         }
     }
 }
