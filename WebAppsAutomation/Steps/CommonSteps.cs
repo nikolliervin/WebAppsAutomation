7d1b2e8d128a1b978e80ba0403529c46b9245b2e
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-16 19:03:31 +0200

    Implemented the generic method to fire the web apps URL

--- WebAppsAutomation/Steps/CommonSteps.cs
+++ WebAppsAutomation/Steps/CommonSteps.cs
@@ -1,6 +1,19 @@
-﻿namespace WebAppsAutomation.Steps
+﻿using WebAppsAutomation.URLs;
+
+namespace WebAppsAutomation.Steps
 {
-    public class CommonSteps
+
+    public class CommonSteps : WebDriver.WebDriver
     {
+        private readonly WebAppURLs url;
+        public CommonSteps(WebAppURLs _url)
+        {
+            url = _url;
+        }
+
+        public void IHaveLaunchedTheApp(string AppName)
+        {
+            driver.Navigate().GoToUrl(url.AppURL[AppName]);
+        }
     }
 }
