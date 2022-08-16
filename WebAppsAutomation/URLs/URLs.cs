4f2ed670e568cca4397eaf3ae0c33eb3d31d5f95
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-16 18:55:55 +0200

    URLs - Adding all URLs to the dictionary

---
+++ WebAppsAutomation/URLs/URLs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WebAppsAutomation.URLs
+{
+    public class WebAppURLs
+    {
+        public Dictionary<string, string> AppURL = new Dictionary<string, string>()
+        {
+            {"ToDoApp","https://gorgeous-fudge-cc9998.netlify.app/" },
+            {"WeatherApp", "https://marvelous-nougat-1199fd.netlify.app/" },
+            {"Stopwatch","https://courageous-dusk-90082b.netlify.app/" },
+            {"TruthOrDare","https://boisterous-froyo-aacb2d.netlify.app/" }
+        };
+    }
+}
